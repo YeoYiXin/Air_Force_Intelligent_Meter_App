c2b553ef2544c96f39d3c968611db0cefea0fde0
YeoYiXin <sara.yeo.yixin@gmail.com>
2024-04-28 23:28:03 +0800

    updated on manager

--- lib/src/pages/manager/manager.dart
+++ lib/src/pages/manager/manager.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:intelligent_meter_app/src/pages/manager/homepage/add/result.dart';
+import 'package:intelligent_meter_app/src/pages/manager/homepage/meter_movement/meter_movement.dart';
+import 'package:intelligent_meter_app/src/pages/manager/homepage/order_updates/order_updates.dart';
+import 'package:intelligent_meter_app/src/pages/manager/homepage/tasks/tasks_carousel.dart';
 import 'package:intelligent_meter_app/src/pages/manager/navigation/bottom_nav.dart';
 import 'package:intelligent_meter_app/src/pages/manager/profile/manager_profile.dart';
 import 'package:intelligent_meter_app/src/pages/manager/homepage/notification/notification.dart';
@@ -76,33 +79,55 @@ class _ManagerState extends State<Manager> {
           ),
         ),
         body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              NotificationPage(),
-              Gap(10),
-              Padding(
-                padding: EdgeInsets.only(left: 10, right: 10),
-                child: Text(
-                  "Welcome Back, \nKai Min",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromRGBO(14,102,129,1),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                NotificationPage(),
+                Gap(10),
+                Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: Text(
+                    "Welcome Back, \nKai Min",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(14, 102, 129, 1),
+                    ),
+                    textAlign: TextAlign.start,
                   ),
-                  textAlign: TextAlign.start,
                 ),
-              ),
-              Gap(10),
-              Result(),
-              Gap(10),
-              
-            ],
-          )
-        ),
+                Gap(10),
+                Result(),
+                Gap(15),
+                Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Text("My tasks",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                Flexible(
+                  child: TasksListManager(),
+                ),
+                Gap(15),
+                Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Text("Order updates",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                Flexible(
+                  child: Order_Updates(),
+                ),
+                Gap(15),
+                Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Text("Monitor meter movements",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                Flexible(child: MeterMovement(),)
+              ],
+            )),
         bottomNavigationBar: BottomNav(),
       ),
     );
